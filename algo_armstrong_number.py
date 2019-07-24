1cce0b083706c8a27f87d7d50cb8440b95cca05d
Nishant Maheshwari <37651902+rootnanu@users.noreply.github.com>
2019-07-25 02:52:37 +0530

    Simple logical algorithm to check armstrong number in python

---
+++ algo_armstrong_number.py
@@ -0,0 +1,16 @@
+#Take input from user.. >>
+tke=(input("Enter the number to check whether it's an Armstrong number or not :: "))
+sumof=0
+temp=tke
+leng=len(tke)
+tke=list(tke)
+#Loop will start to compute power of number with length.. >>
+for i in range(0,leng):
+    tke[i]=int(tke[i])
+    sumof+=tke[i]**leng
+#Print answer to user if number is armstrong or not.. >>
+if sumof==temp:
+    print("YES ! (",temp,") is an Armstrong Number !")
+else:
+    print("NO ! (",temp,") is not an Armstrong Number !")
+exit(0)
